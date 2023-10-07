a3a630ad18f0013d453479d6fb4789494b0312c7
Huast568 <56443740+Huast568@users.noreply.github.com>
2023-10-07 14:45:12 -0400

    Update 1-Purpose.py

--- pages/1-Purpose.py
+++ pages/1-Purpose.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
 
-st.title("Purpose of the App")
-st.write("This app allows users to upload images or videos and perform various operations on them.")
+st.title("ImagineSpace")
+st.write("ImagineSpace is a web app that sparks the imagination of youth and individuals with visual impairment to dream the beauty of space. Our mission is to give everyone the privilege of appreciating the vastness and wonders of the universe. At the end of the day, we all live under the same night sky, and it would be our team’s greatest wish for that sky to be what connects communities and families; maybe inspiring a young mind to become an astronaut, astrophysicist, or astronomer along the way.
+")
+
+#Inserting Background Photo
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://news.mit.edu/sites/default/files/images/202306/MIT-3q-FirstLight-01-press.jpg");
+background-size: cover;
+}
