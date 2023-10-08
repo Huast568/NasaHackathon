7fad78db76aa6d926e4e5e444fdda337c57c8cf4
SebA-R <123127567+SebA-R@users.noreply.github.com>
2023-10-08 14:43:41 -0400

    ui theme

--- pages/1-Purpose.py
+++ pages/1-Purpose.py
@@ -1,13 +1,6 @@
 import streamlit as st
 
 
-st.title("ImagineSpace")
-st.write("ImagineSpace is a web app that sparks the imagination of youth and individuals with visual impairment to dream the beauty of space. Our mission is to give everyone the privilege of appreciating the vastness and wonders of the universe. At the end of the day, we all live under the same night sky, and it would be our team’s greatest wish for that sky to be what connects communities and families; maybe inspiring a young mind to become an astronaut, astrophysicist, or astronomer along the way.")
+st.title("AstroSonic")
+st.write("AstroSonic is a web app that sparks the imagination of youth and individuals with visual impairment to dream the beauty of space. Our mission is to give everyone the privilege of appreciating the vastness and wonders of the universe. At the end of the day, we all live under the same night sky, and it would be our team’s greatest wish for that sky to be what connects communities and families; maybe inspiring a young mind to become an astronaut, astrophysicist, or astronomer along the way.")
 
-#Inserting Background Photo
-st.markdown('''<style>
-.stApp {
-background-image: url("https://news.mit.edu/sites/default/files/images/202306/MIT-3q-FirstLight-01-press.jpg");
-background-size: cover;
-}
-</style>''', unsafe_allow_html=True)
