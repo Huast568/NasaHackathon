30425864f856f3fe3be94f87586102288ba197b0
Huast568 <56443740+Huast568@users.noreply.github.com>
2023-10-08 23:31:35 -0400

    Update 1-Citizen Science.py

--- pages/1-Citizen Science.py
+++ pages/1-Citizen Science.py
@@ -3,4 +3,8 @@ import streamlit as st
 
 st.title("Citizen Science")
 st.write("Our app also aims to inspire citizen scientists with the following features:")
+st.write(" ")
+st.write(" ")
+st.write("1. Accessible Data and Tools:")
+st.write(" ")
 
