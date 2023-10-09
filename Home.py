5ae90aac36c82712a8d3f1f113b312d9d3721d7c
Huast568 <56443740+Huast568@users.noreply.github.com>
2023-10-08 23:28:32 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.title("SonicSpace")
-
-st.write("
+st.write("SonicSpace is a web app that sparks the imagination of youth and individuals with visual impairment to dream the beauty of space. Our mission is to give everyone the privilege of appreciating the vastness and wonders of the universe. At the end of the day, we all live under the same night sky, and it would be our team’s greatest wish for that sky to be what connects communities and families; maybe inspiring a young mind to become an astronaut, astrophysicist, or astronomer along the way.")
 
 
